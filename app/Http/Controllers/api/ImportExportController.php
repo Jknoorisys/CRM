705e90a7e2e222e46fd3b8fa60953235bc25cfed
Javeriya <javeriya.noorisys@gmail.com>
2024-01-08 17:18:51 +0530

    Fix file path for exporting contacts

--- app/Http/Controllers/api/ImportExportController.php
+++ app/Http/Controllers/api/ImportExportController.php
@@ -16,7 +16,7 @@ class ImportExportController extends Controller
     public function exportContacts() {
         try {
             $name = time();
-            $file = Excel::store(new ExportContact, 'assets/uploads/contacts/'.$name.'.xlsx');
+            $file = Excel::store(new ExportContact, $name.'.xlsx', 'assets/uploads/contacts');
             if (!empty($file)) {
                 $uploadUrl = 'assets/uploads/contacts/'.$name.'.xlsx';
                 return response()->json([
