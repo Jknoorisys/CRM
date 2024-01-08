ed644d0d9a0022321965f0c463df6caf3db8ab64
Javeriya <javeriya.noorisys@gmail.com>
2024-01-08 16:52:14 +0530

    Fix uploadUrl path in exportContacts method

--- app/Http/Controllers/api/ImportExportController.php
+++ app/Http/Controllers/api/ImportExportController.php
@@ -18,7 +18,7 @@ class ImportExportController extends Controller
             $name = time();
             $file = Excel::store(new ExportContact, $name.'.xlsx', 'assets_uploads_contacts');
             if (!empty($file)) {
-                $uploadUrl = 'public/assets/uploads/contacts/'.$name.'.xlsx';
+                $uploadUrl = 'assets/uploads/contacts/'.$name.'.xlsx';
                 return response()->json([
                     'status'    => 'success',
                     'message'   => trans('msg.download.success'),
